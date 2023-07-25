680fa068d954063b4304d4d225cd140e3c429e7d
ワイルドコードペンギン <wild.code.penguin@gmail.com>
2023-07-25 17:53:09 +0900

    コミットメッセージ

---
+++ BattleFamily/code3.js
@@ -0,0 +1,18 @@
+gdjs._12508_12473_12496_12488_12523Code = {};
+
+
+gdjs._12508_12473_12496_12488_12523Code.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs._12508_12473_12496_12488_12523Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs._12508_12473_12496_12488_12523Code.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['_12508_12473_12496_12488_12523Code'] = gdjs._12508_12473_12496_12488_12523Code;
